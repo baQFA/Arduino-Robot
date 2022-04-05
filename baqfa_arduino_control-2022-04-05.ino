ef5346aa247df5d07ad4c5abe08eaace560e970c
Pascal Frey <pascal.frey@gmail.com>
2022-04-05 14:37:57 +0200

    Program Code for Arduino Uno BaQFA

--- baqfa_arduino_control-2022-04-05.ino
+++ baqfa_arduino_control-2022-04-05.ino
@@ -1,7 +1,8 @@
 /*
-Control the BaQFA setup for 1 plate with an Arduino Uno, using an optocoupler 
-for the camera shutter control, two SG90 servo motors, and a WS2812B LED Strip
-ligthing.
+Control the BaQFA setup for 1 plate with an Arduino Uno, using an optocoupler
+or transistor for the camera shutter control, two SG90 micro servo motors, and
+a WS2812B LED strip for ligthing.
+Author: Pascal M. Frey
 */
 
 
@@ -9,15 +10,18 @@ ligthing.
 #include <Servo.h>
 
 #define LED_PIN     12
-#define LED_COUNT    20 //define the real number of LEDs according to the setup
+#define LED_COUNT    52 //define the real number of LEDs according to the setup
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 Servo servo1; // right side servo when looking from the direction of the camera
 Servo servo2; // left side servo when looking from the direction of the camera
 
-int pos1 = 0; // positioning of first servo
-int pos2 = 90; // use for positioning second servo
+// servo positioning
+int open_position = 100;
+int close_position = 5;
+int pos1 = close_position; // positioning of first servo
+int pos2 = open_position; // use for positioning second servo
 
 // the interval between pictures during BaQFA, 5000ms are needed to take the picture 
 int interval = 1795000;
@@ -30,20 +34,21 @@ void setup() {
 
   strip.begin();
   strip.show();
+
+  pinMode(A4 , OUTPUT); // define the A4 analog pin for the camera shutter control
   
   servo1.attach(2);
   servo2.attach(3);
   //delay(300);
 
   // open lid
-  servo1.write(0);
-  servo2.write(90);
+  servo1.write(close_position);
+  servo2.write(open_position);
 
-  delay(20000); // wait 30 seconds so plate can be put in place
+  delay(20000); // wait 20 seconds so plate can be put in place
 
   close_lid();
-
-  pinMode(A4 , OUTPUT); // define the A4 analog pin for the camera shutter control
+  delay(5000);
 
   testpicture(3);
   
@@ -57,7 +62,7 @@ void setup() {
 void loop() {
 
     makepicture();
-    delay(interval);
+    delay(1795000);
   
 }
 
@@ -69,14 +74,14 @@ void open_lid() {
     servo1.attach(2);
     servo2.attach(3);
     delay(50);
-    pos2 = 0;
+    pos2 = close_position;
   
     //open lid at speed of 1° every 15ms
-    for (pos1 = 90; pos1 >= 0; pos1--) {
+    for (pos1 = open_position; pos1 >= close_position; pos1--) {
     servo1.write(pos1);
       servo2.write(pos2);
       pos2++;
-      delay(15);
+      delay(10);
     }
   
     delay(50);
@@ -92,14 +97,14 @@ void close_lid() {
     servo1.attach(2);
     servo2.attach(3);
     delay(50);
-    pos2 = 90;
+    pos2 = open_position;
   
     //close lid at speed of 1° every 15ms
-    for (pos1 = 0; pos1 <= 90; pos1++) {
+    for (pos1 = close_position; pos1 <= open_position; pos1++) {
     servo1.write(pos1);
       servo2.write(pos2);
       pos2--;
-      delay(15);
+      delay(10);
     }
     
     delay(50);
@@ -131,19 +136,18 @@ void lights_off() {
 
 
 void makepicture() {
- 
-  lights_on();
-  
+
     open_lid();
-    
+    delay(1000); // the delay is needed to avoid power spikes rebooting the Arduino when the whole system is powered through the Arduino Uno board
+    lights_on();
+    delay(500);
     digitalWrite(A4 , HIGH); //trigger the OptoCoupler for camera shutter
     delay(500);
     digitalWrite(A4 , LOW); //stop triggering OptoCoupler
-    delay(4500); //wait until exposure time for low-light picture is over
-  
-  close_lid();
-    
+    delay(4500); //wait until exposure time for low-light picture is over  
     lights_off();
+    delay(1000);
+    close_lid();
 }
 
 
